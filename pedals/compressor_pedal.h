125343fe7d35aef49a3e4d53a10f19e2bca55c5e
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-03 14:07:12 -0700

    improved the compressor pedal

--- pedals/compressor_pedal.h
+++ pedals/compressor_pedal.h
@@ -3,23 +3,33 @@
 
 #include "pedal.h"
 #include "pedal_registry.h"
+#include "q/fx/dynamic.hpp"
+#include "q/fx/envelope.hpp"
 #include "signal.h"
 
-// Attempts to compress the signal within a specific amplitude range.
-//
-// Instead of applying hard clipping outside of the range, this pedal does soft
-// clipping (allowing the signal to go outside the range, but damping the
-// portion that goes outside).
+// Compresses the input signal.
 class CompressorPedal : public Pedal {
  public:
   SignalType Transform(SignalType signal) override {
-    constexpr SignalType kThreshold = 0.0001;
-    auto diff = std::abs(signal) - kThreshold;
-    float signal_sign = signal < 0 ? -1 : 1;
-    return diff > 0 ? signal_sign * kThreshold + (signal / 4) : signal;
+    // Take the absolute value of the signal here because we only care about its
+    // amplitude, not its sign, when performing compression.
+    SignalType compression_gain =
+        SignalType(compressor_(envelope_tracker_(std::abs(signal))));
+
+    // TODO: Consider adding post gain here to makeup for volume drop due to
+    // compression.
+    return signal * compression_gain;
   }
 
   std::string Describe() override { return "compressor"; }
+
+ private:
+  cycfi::q::compressor compressor_{/* threshold= */ 0.1,
+                                   /* ratio= */ 0.1};
+
+  cycfi::q::envelope_follower envelope_tracker_{/* attack= */ 400 * 1e-3,
+                                                /* release= */ 400 * 1e-3,
+                                                /* sample_rate= */ 44100};
 };
 
 REGISTER_PEDAL("compressor",
