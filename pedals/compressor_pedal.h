d31a2d1c68f52a219d6f2f5e9022a9d49fc55d20
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-10 15:04:01 -0700

    expose compressor params

--- pedals/compressor_pedal.h
+++ pedals/compressor_pedal.h
@@ -10,6 +10,12 @@
 // Compresses the input signal.
 class CompressorPedal : public Pedal {
  public:
+  CompressorPedal(double attack_seconds, double release_seconds)
+      : attack_seconds_(attack_seconds),
+        release_seconds_(release_seconds),
+        envelope_tracker_(attack_seconds_, release_seconds_,
+                          /* sample_rate= */ 44100) {}
+
   SignalType Transform(SignalType signal) override {
     // Take the absolute value of the signal here because we only care about its
     // amplitude, not its sign, when performing compression.
@@ -24,19 +30,39 @@ class CompressorPedal : public Pedal {
   PedalInfo Describe() override {
     PedalInfo info;
     info.name = "Compressor";
+
+    info.knobs = {
+        PedalKnob{
+            .name = "attack", .value = attack_seconds_, .tweak_amount = 0.1},
+        PedalKnob{
+            .name = "release", .value = release_seconds_, .tweak_amount = 0.1},
+    };
     return info;
   }
 
+  void AdjustKnob(const PedalKnob& pedal_knob) override {
+    if (pedal_knob.name == "attack") {
+      attack_seconds_ = pedal_knob.value;
+    } else if (pedal_knob.name == "release") {
+      release_seconds_ = pedal_knob.value;
+    }
+
+    envelope_tracker_ =
+        cycfi::q::envelope_follower(attack_seconds_, release_seconds_,
+                                    /* sample_rate= */ 44100);
+  }
+
  private:
+  double attack_seconds_;
+  double release_seconds_;
   cycfi::q::compressor compressor_{/* threshold= */ 0.1,
                                    /* ratio= */ 0.1};
-
-  cycfi::q::envelope_follower envelope_tracker_{/* attack= */ 400 * 1e-3,
-                                                /* release= */ 400 * 1e-3,
-                                                /* sample_rate= */ 44100};
+  cycfi::q::envelope_follower envelope_tracker_;
 };
 
-REGISTER_PEDAL("Compressor",
-               []() { return std::unique_ptr<Pedal>(new CompressorPedal()); });
+REGISTER_PEDAL("Compressor", []() {
+  return std::unique_ptr<Pedal>(new CompressorPedal(
+      /* attack_seconds= */ 0.4, /* release_seconds= */ 0.4));
+});
 
 #endif /* COMPRESSOR_PEDAL_H */
