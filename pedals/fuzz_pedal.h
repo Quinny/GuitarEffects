4b4f7c5400c075b239a6553fad665acd08cc94aa
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-10 14:42:43 -0700

    expose the pre-gain knob from the fuzz pedal

--- pedals/fuzz_pedal.h
+++ pedals/fuzz_pedal.h
@@ -9,12 +9,13 @@
 // input signal.
 class FuzzPedal : public Pedal {
  public:
+  FuzzPedal(double pre_gain) : pre_gain_(pre_gain) {}
+
   SignalType Transform(SignalType signal) override {
     constexpr static SignalType kMax = 0.9;
-    constexpr static SignalType kPreGain = 0.4;
 
     int input_sign = signal > 0 ? 1 : -1;
-    SignalType pre_gained = signal + (input_sign * kPreGain);
+    SignalType pre_gained = signal + (input_sign * pre_gain_);
 
     if (pre_gained < -kMax) return -kMax;
     if (pre_gained > kMax) return kMax;
@@ -24,10 +25,21 @@ class FuzzPedal : public Pedal {
   PedalInfo Describe() override {
     PedalInfo info;
     info.name = "Fuzz";
+    info.knobs = {
+        PedalKnob{.name = "pre-gain", .value = pre_gain_, .tweak_amount = 0.1}};
     return info;
   }
+
+  void AdjustKnob(const PedalKnob& pedal_knob) override {
+    pre_gain_ = pedal_knob.value;
+  }
+
+ private:
+  double pre_gain_;
 };
 
-REGISTER_PEDAL("Fuzz", []() { return std::unique_ptr<Pedal>(new FuzzPedal); });
+REGISTER_PEDAL("Fuzz", []() {
+  return std::unique_ptr<Pedal>(new FuzzPedal(/* pre_gain = */ 0.4));
+});
 
 #endif /* FUZZ_PEDAL_H */
