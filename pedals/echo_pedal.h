4bb193c69bcbf564883a8f60671f97a18d731e28
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-10 14:52:16 -0700

    expose echo pedal params

--- pedals/echo_pedal.h
+++ pedals/echo_pedal.h
@@ -15,12 +15,14 @@
 // signal back into the buffer instead of overwriting with the input.
 class EchoPedal : public Pedal {
  public:
-  EchoPedal(int buffer_size) : echo_buffer_(buffer_size, 0) {}
+  EchoPedal(double echo_seconds, double decay_factor)
+      : echo_seconds_(echo_seconds),
+        decay_factor_(decay_factor),
+        echo_buffer_(44100 * echo_seconds_, 0) {}
 
   SignalType Transform(SignalType signal) override {
-    static constexpr int kDecayFactor = 4;
     echo_buffer_[echo_index_] =
-        (echo_buffer_[echo_index_] + signal) / kDecayFactor;
+        (echo_buffer_[echo_index_] + signal) / decay_factor_;
     echo_index_ = (echo_index_ + 1) % echo_buffer_.size();
     return signal + echo_buffer_[echo_index_];
   }
@@ -28,15 +30,38 @@ class EchoPedal : public Pedal {
   PedalInfo Describe() override {
     PedalInfo info;
     info.name = "Echo";
+    info.knobs = {
+        PedalKnob{.name = "echo_seconds",
+                  .value = echo_seconds_,
+                  .tweak_amount = 0.1},
+        PedalKnob{.name = "decay_factor",
+                  .value = decay_factor_,
+                  .tweak_amount = 0.5},
+    };
     return info;
   }
 
+  void AdjustKnob(const PedalKnob& pedal_knob) override {
+    if (pedal_knob.name == "echo_seconds") {
+      echo_seconds_ = pedal_knob.value;
+    } else if (pedal_knob.name == "decay_factor") {
+      decay_factor_ = pedal_knob.value;
+    }
+
+    echo_index_ = 0;
+    echo_buffer_ = std::vector<SignalType>(44100 * echo_seconds_, 0);
+  }
+
  private:
-  std::vector<SignalType> echo_buffer_;
+  double echo_seconds_;
+  double decay_factor_;
   int echo_index_ = 0;
+  std::vector<SignalType> echo_buffer_;
 };
 
-REGISTER_PEDAL("Echo",
-               []() { return std::unique_ptr<Pedal>(new EchoPedal(20000)); });
+REGISTER_PEDAL("Echo", []() {
+  return std::unique_ptr<Pedal>(
+      new EchoPedal(/* echo_seconds= */ 0.5, /* decay_factor= */ 4));
+});
 
 #endif /* ECHO_PEDAL_H */
