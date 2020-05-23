d916f1c349c1846e44f83940b282aaef6ed36bac
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-23 11:06:52 -0700

    Make the fuzz pedal select the clipping threshold adaptively so that tuning the pre gain and threshold isn't such a pita

--- pedals/fuzz_pedal.h
+++ pedals/fuzz_pedal.h
@@ -9,17 +9,28 @@
 // input signal.
 class FuzzPedal : public Pedal {
  public:
-  FuzzPedal(double pre_gain) : pre_gain_(pre_gain) {}
+  FuzzPedal() : signal_window_(44100 * window_size_seconds_, 0) {}
 
   SignalType Transform(SignalType signal) override {
-    int input_sign = signal > 0 ? 1 : -1;
-    SignalType pre_gained = signal + (input_sign * pre_gain_);
+    // Adpatively maintain a clip threshold based on the average signal observed
+    // over the duration of the window. This removes the need to tune the fuzz
+    // to work with the gain level of the input.
 
-    if (pre_gained < -clip_threshold_) {
-      return -clip_threshold_;
+    // Remove the last sample from the window.
+    window_sum_ -= signal_window_[window_end_];
+    // Add the current sample.
+    signal_window_[window_end_] = std::abs(signal);
+    window_sum_ += signal_window_[window_end_];
+    // Move the window ahead.
+    window_end_ = (window_end_ + 1) % signal_window_.size();
+
+    // Select the threshold as the average amplitude over the window.
+    SignalType clip_threshold = window_sum_ / signal_window_.size();
+    if (signal < -clip_threshold) {
+      return -clip_threshold;
     }
-    if (pre_gained > clip_threshold_) {
-      return clip_threshold_;
+    if (signal > clip_threshold) {
+      return clip_threshold;
     }
 
     return signal;
@@ -28,30 +39,30 @@ class FuzzPedal : public Pedal {
   PedalInfo Describe() override {
     PedalInfo info;
     info.name = "Fuzz";
-    info.knobs = {
-        PedalKnob{.name = "pre_gain", .value = pre_gain_, .tweak_amount = 0.1},
-        PedalKnob{.name = "clip_threshold",
-                  .value = clip_threshold_,
-                  .tweak_amount = 0.1},
-    };
+    info.knobs = {PedalKnob{.name = "window_size_seconds",
+                            .value = window_size_seconds_,
+                            .tweak_amount = 0.2}};
     return info;
   }
 
   void AdjustKnob(const PedalKnob& pedal_knob) override {
-    if (pedal_knob.name == "pre_gain") {
-      pre_gain_ = pedal_knob.value;
-    } else if (pedal_knob.name == "clip_threshold") {
-      clip_threshold_ = pedal_knob.value;
+    if (pedal_knob.name == "window_size_seconds") {
+      window_size_seconds_ = pedal_knob.value;
     }
+
+    signal_window_ = std::vector<SignalType>(44100 * window_size_seconds_, 0);
+    window_end_ = 0;
+    window_sum_ = 0;
   }
 
  private:
-  double pre_gain_;
-  double clip_threshold_ = 0.9;
+  double window_size_seconds_ = 0.2;
+  std::vector<SignalType> signal_window_;
+  SignalType window_sum_ = 0;
+  int window_end_ = 0;
 };
 
-REGISTER_PEDAL("Fuzz", []() {
-  return std::unique_ptr<Pedal>(new FuzzPedal(/* pre_gain = */ 0.4));
-});
+REGISTER_PEDAL("Fuzz",
+               []() { return std::unique_ptr<Pedal>(new FuzzPedal()); });
 
 #endif /* FUZZ_PEDAL_H */
