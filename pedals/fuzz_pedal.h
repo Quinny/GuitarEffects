fb087ed1b1fc5a6d1763726a1d50a0303b5ad03d
Quinn Perfetto <thequinnftw@gmail.com>
2020-05-31 15:02:52 -0700

    rework the fuzz to a square wave shaper

--- pedals/fuzz_pedal.h
+++ pedals/fuzz_pedal.h
@@ -1,65 +1,60 @@
 #ifndef FUZZ_PEDAL_H
 #define FUZZ_PEDAL_H
 
+#include "q/support/base.hpp"
+#include "q/support/frequency.hpp"
+#include "q/support/literals.hpp"
+
+#include "fx/wave_shaper.h"
 #include "pedal.h"
 #include "pedal_registry.h"
+#include "q/fx/biquad.hpp"
 #include "signal_type.h"
 
 // A pedal which creates a fuzz effect by applying hard clipping to the
 // input signal.
 class FuzzPedal : public Pedal {
  public:
-  FuzzPedal() : signal_window_(44100 * window_size_seconds_, 0) {}
-
   SignalType Transform(SignalType signal) override {
-    // Adpatively maintain a clip threshold based on the average signal observed
-    // over the duration of the window. This removes the need to tune the fuzz
-    // to work with the gain level of the input.
-
-    // Remove the last sample from the window.
-    window_sum_ -= signal_window_[window_end_];
-    // Add the current sample.
-    signal_window_[window_end_] = std::abs(signal);
-    window_sum_ += signal_window_[window_end_];
-    // Move the window ahead.
-    window_end_ = (window_end_ + 1) % signal_window_.size();
-
-    // Select the threshold as the average amplitude over the window.
-    SignalType clip_threshold = window_sum_ / signal_window_.size();
-    if (signal < -clip_threshold) {
-      return -clip_threshold;
-    }
-    if (signal > clip_threshold) {
-      return clip_threshold;
-    }
-
+    signal = shaper_(signal);
+    signal = bandpass_(signal * frequency_multipler_);
     return signal;
   }
 
   PedalInfo Describe() override {
     PedalInfo info;
     info.name = "Fuzz";
-    info.knobs = {PedalKnob{.name = "window_size_seconds",
-                            .value = window_size_seconds_,
-                            .tweak_amount = 0.2}};
+    info.knobs = {
+        PedalKnob{.name = "boost", .value = boost_, .tweak_amount = 0.1},
+        PedalKnob{.name = "frequency_multipler",
+                  .value = frequency_multipler_,
+                  .tweak_amount = 0.1},
+    };
     return info;
   }
 
   void AdjustKnob(const PedalKnob& pedal_knob) override {
-    if (pedal_knob.name == "window_size_seconds") {
-      window_size_seconds_ = pedal_knob.value;
+    if (pedal_knob.name == "boost") {
+      boost_ = pedal_knob.value;
+    } else if (pedal_knob.name == "frequency_multipler") {
+      frequency_multipler_ = pedal_knob.value;
     }
 
-    signal_window_ = std::vector<SignalType>(44100 * window_size_seconds_, 0);
-    window_end_ = 0;
-    window_sum_ = 0;
+    shaper_ = {[this](int x) { return Curve(x); }, 4096};
   }
 
  private:
-  double window_size_seconds_ = 0.2;
-  std::vector<SignalType> signal_window_;
-  SignalType window_sum_ = 0;
-  int window_end_ = 0;
+  SignalType Curve(int x) {
+    auto y = std::tanh(x) * boost_;
+    auto clipped = std::max<double>(std::min<double>(y, 1), -1);
+    return clipped;
+  }
+
+  double boost_ = 0.7;
+  double frequency_multipler_ = 2;
+
+  cycfi::q::bandpass_csg bandpass_{1200, 44100};
+  WaveShaper shaper_{[this](int x) { return Curve(x); }, 4096};
 };
 
 REGISTER_PEDAL("Fuzz",
