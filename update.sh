03cf44a7b8cd8ef6b60899a46e5a34c8bad39b18
Quinn Perfetto <thequinnftw@gmail.com>
2021-04-25 11:44:30 -0700

    bash plz

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 git fetch
 git reset --hard HEAD
 update_status=$(git merge origin/master)
-if ["$update_status" = "Already up to date."]; then
+if ["${update_status}" = "Already up to date."]; then
   echo "Already up to date, not rebuilding";
 else
   make server
